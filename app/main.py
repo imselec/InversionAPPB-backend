e9f1958c7f679929b6e6b02f85c519c6f328bb53
Ivan Weisz <imselec@gmail.com>
2026-02-06 16:42:42 +0100

    Add working monthly investment plan endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+
+from app.api.system import router as system_router
 from app.api.portfolio import router as portfolio_router
 from app.api.investments import router as investments_router
-from app.api.system import router as system_router
 
 app = FastAPI(title="InversionAPP API")
 
 app.include_router(system_router, prefix="/system", tags=["system"])
 app.include_router(portfolio_router, prefix="/portfolio", tags=["portfolio"])
 app.include_router(investments_router, prefix="/investments", tags=["investments"])
-
